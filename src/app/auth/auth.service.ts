b65dc445e3fd5593e861566d48fd3c26b35ac7d0
irynastsiarzhanava <iryna.stsiarzhanava@gmail.com>
2019-03-02 13:10:00 +0100

    Adding a Logout button

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,9 +2,18 @@ import * as firebase from 'firebase';
 import { tokenKey } from '@angular/core/src/view';
 
 export class AuthService {
-    token ='';
+    token: string;
     signupUser(email: string, password: string) {
         firebase.auth().createUserWithEmailAndPassword(email, password)
+            .then(
+                response => console.log(response)
+            )             
+            .catch(
+                error => console.log(error)
+            )
+    }
+    signinUser(email: string, password: string) {
+        firebase.auth().signInWithEmailAndPassword(email, password)          
             .then(
                 response => {
                     firebase.auth().currentUser.getIdToken()
@@ -12,21 +21,15 @@ export class AuthService {
                             (token: string) => this.token = token
                         )
                 }
-            )
-                
+            )               
             .catch(
                 error => console.log(error)
             )
     }
-    signinUser(email: string, password: string) {
-        firebase.auth().signInWithEmailAndPassword(email, password)
-            .then(
-                response => console.log(response)
-            )
-                
-            .catch(
-                error => console.log(error)
-            )
+
+    logout() {
+        firebase.auth().signOut();
+        this.token = null;
     }
 
     getToken() {
